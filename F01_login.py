158234225ec0309813d1f8514b3c172ea3d8b91f
Filbert <92862064+Filbert88@users.noreply.github.com>
2023-04-14 23:16:28 +0700

    Update F01_login.py

--- F01_login.py
+++ F01_login.py
@@ -1,21 +1,22 @@
 from load_data import *
-
-# (F01) Mendefinisikan Fungsi Login 
-def login():
-    # Menerima masukan usernam dan password dari pengguna ke variabel uname dan passs 
-    uname = input("Username: ")
-    passs = input("Password: ")
-    # Melakukan pengecekan apakah masukan pengguna terdaftar pada "user.csv" atau tidakl
-    for i in range (lenSendiri(username_arr,102)):
-        if uname == username_arr[i]:
-            if passs == password_arr[i]:
-                print()
-                print("Selamat datang, "+ str(uname)+"!")
-                print("Masukkan command “help” untuk daftar command yang dapat kamu panggil.")
-                return role_arr[i]
-            else:   # jika password yang dimasukkan salah
-                print("Password salah!")
-                return ""
-    # jika username tidak terdapat pada "user.csv"
-    print("Username tidak terdaftar!")
-    return ""
+def login(uname):
+    if role=="":
+        # Menerima masukan usernam dan password dari pengguna ke variabel uname dan passs 
+        uname = input("Username: ")
+        passs = input("Password: ")
+        # Melakukan pengecekan apakah masukan pengguna terdaftar pada "user.csv" atau tidak
+        for i in range (lenSendiri(username_arr,102)):
+            if uname == username_arr[i]:
+                if passs == password_arr[i]:
+                    print("\nSelamat datang, "+ str(uname)+"!")
+                    print("Masukkan command “help” untuk daftar command yang dapat kamu panggil.")
+                    return role_arr[i],uname
+                else:   # jika password yang dimasukkan salah
+                    print("\nPassword salah!")
+                    return "",""
+        # jika username tidak terdapat pada "user.csv"
+        print("\nUsername tidak terdaftar!")
+        return "",""
+    else:
+        print(f"Login gagal!\nAnda telah login dengan username {uname}, silahkan lakukan \"logout\" sebelum melakukan login kembali.")
+        return  "",""
