6cb1cbaaa9fcd03c3043dff10590ebfb2eba758e
Filbert <92862064+Filbert88@users.noreply.github.com>
2023-04-14 23:14:34 +0700

    Update F04_hapusjin.py

--- F04_hapusjin.py
+++ F04_hapusjin.py
@@ -1,3 +1,16 @@
 # (F04) Mendefinisikan Fungsi Hapus Jin
 def hapusjin():
-    pass
+    unamejin = input("Masukkan username: ")
+    for i in range(102):
+        if username_arr[i]==unamejin:
+            valid=True
+            index=i
+            break
+    else: valid=False
+    if valid==False :
+        print("Tidak ada jin dengan username tersebut.")
+    else :
+        username_arr[index]=None
+        role_arr[index]=None
+        password_arr[index]=None
+        print(f"Jin dengan username {unamejin} telah  dihapus")
