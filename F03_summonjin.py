6910376ae5fd39c347caa91e19d23b9bfc0eb32f
Filbert <92862064+Filbert88@users.noreply.github.com>
2023-04-14 23:15:01 +0700

    Update F03_summonjin.py

--- F03_summonjin.py
+++ F03_summonjin.py
@@ -1,13 +1,12 @@
 # (F03) Mendefinisikan Fungsi Summon Jin
 def summonjin():
-    global username_arr, password_arr, unamejin, jenis_jin
+    global username_arr, password_arr, unamejin, jenis_jin, role_arr
     if lenSendiri(username_arr,102) == 102 :
         print("Jumlah jin telah maksimal! (100 jin). Bandung tidak dapat men-summon lebih dari itu")
     else :
         print("Jenis jin yang dapat dipanggil:")
         print(" (1) Pengumpul - Bertugas mengumpulkan bahan bangunan")
         print(" (2) Pembangun - Bertugas membangun candi")
-
         n = int(input("\nMasukkan nomor jenis jin yang ingin dipanggil: "))
         while n != 1 and n != 2 :
             if n < 1 or n > 2 :
@@ -19,18 +18,32 @@ def summonjin():
             else: jenis_jin = "Pembangun"
             print(f"\nMemilih jin \"{jenis_jin}\".")
             unamejin = str(input("\nMasukkan username jin: "))
-            uname_exists = unamejin in username_arr
-            while uname_exists:
+            valid = False
+            for i in range(102) :
+                if unamejin == username_arr[i] :
+                    valid = True
+                    break
+            while valid :
+                print(username_arr)
                 print(f"Username \"{unamejin}\" sudah diambil!")
                 unamejin = str(input("\nMasukkan username jin: "))
-                uname_exists = unamejin in username_arr
+                for i in range(102) :
+                    if unamejin == username_arr[i] :
+                        valid = True
+                        break
+                else:valid=False
             passjin = str(input("Masukkan password jin: "))
-            x = len(passjin) 
-            while not (5 <= x <= 25):
-                print("\npassword panjangnya harus 5-25 karakter!")
-                passjin = str(input("\nMasukkan password jin: "))
-                x = len(passjin)
-            username_arr = appendSendiri(username_arr,unamejin)
-            password_arr = appendSendiri(password_arr,passjin)
+            if valid == False :
+                x = len(passjin) 
+                while not (5 <= x <= 25):
+                    print("\npassword panjangnya harus 5-25 karakter!")
+                    passjin = str(input("\nMasukkan password jin: "))
+                    x = len(passjin)
+            for i in range (102):
+                if username_arr[i]==None:
+                    username_arr[i]=unamejin
+                    password_arr[i]=passjin
+                    role_arr[i]=jenis_jin
+                    break
             print("\nMengumpulkan sesajen...\nMenyerahkan sesajen...\nMembacakan mantra...")
             print(f"\nJin {unamejin} berhasil dipanggil!") 
