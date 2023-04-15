36ef6009dd82802579bf5fd15b1265ae82a4edbd
Filbert <92862064+Filbert88@users.noreply.github.com>
2023-04-15 16:07:58 +0700

    Update F16_exit.py

--- F16_exit.py
+++ F16_exit.py
@@ -1,1 +1,10 @@
+from main import role
 # (F16) Mendefinisikan Fungsi Exit
+def exit() :
+    inp = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ")
+    while (inp != "y" and inp != "n" and inp != "Y" and inp != "N"):
+        inp = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ")
+    if inp == "n" or inp == "N" :
+        pass
+    else : 
+      save()
