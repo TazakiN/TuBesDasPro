cc8ec97ae05f2aa5cf1c8df5f00c475ccded674a
Filbert <92862064+Filbert88@users.noreply.github.com>
2023-04-15 16:05:58 +0700

    Update commands.py

--- commands.py
+++ commands.py
@@ -27,63 +27,64 @@ def run(prompt) -> None:
         else:
             print("summonjin hanya dapat diakses oleh akun Bandung Bondowoso.")
     elif prompt == "hapusjin":
-            if role == "bandung_bondowoso":
-                hapusjin()
-            else:
+        if role == "bandung_bondowoso":
+            hapusjin()
+        else:
                 print("hapusjin hanya dapat diakses oleh akun Bandung Bondowoso.")
     elif prompt == "ubahjin":
-            if role == "bandung_bondowoso":
-                ubahjin()
-            else:
-                print("ubahjin hanya dapat diakses oleh akun Bandung Bondowoso.")
+        if role == "bandung_bondowoso":
+            ubahjin()
+        else:
+            print("ubahjin hanya dapat diakses oleh akun Bandung Bondowoso.")
     elif prompt == "bangun":
-            if role == "jin_pembangun":
-                bangun()
-            else:
-                print("bangun hanya dapat diakses oleh akun Jin Pembangun.")
+        if role == "jin_pembangun":
+            bangun()
+        else:
+            print("bangun hanya dapat diakses oleh akun Jin Pembangun.")
     elif prompt == "kumpul":
-            if role == "jin_pengumpul":
-                kumpul()
-            else:
-                print("kumpul hanya dapat diakses oleh akun Jin Pengumpul.")
+        if role == "jin_pengumpul":
+            kumpul()
+        else:
+            print("kumpul hanya dapat diakses oleh akun Jin Pengumpul.")
     elif prompt == "batchkumpul":
-            if role == "bandung_bondowoso":
-                batchkumpul()
-            else:
-                print("batchkumpul hanya dapat diakses oleh akun Bandung Bondowoso.")
+        if role == "bandung_bondowoso":
+            batchkumpul()
+        else:
+            print("batchkumpul hanya dapat diakses oleh akun Bandung Bondowoso.")
     elif prompt == "batchbangun":
-            if role == "bandung_bondowoso":
-                batchbangun()
-            else:
-                print("batchbangun hanya dapat diakses oleh akun Bandung Bondowoso.")
+        if role == "bandung_bondowoso":
+            batchbangun()
+        else:
+            print("batchbangun hanya dapat diakses oleh akun Bandung Bondowoso.")
     elif prompt == "laporanjin":
-            if role == "bandung_bondowoso":
-                laporanjin()
-            else:
-                print("laporanjin hanya dapat diakses oleh akun Bandung Bondowoso.")
+        if role == "bandung_bondowoso":
+            laporanjin()
+        else:
+            print("laporanjin hanya dapat diakses oleh akun Bandung Bondowoso.")
     elif prompt ==  "laporancandi":
-            if role == "bandung_bondowoso":
-                laporancandi()
-            else:
-                print("laporancandi hanya dapat diakses oleh akun Bandung Bondowoso.")
+        if role == "bandung_bondowoso":
+            laporancandi()
+        else:
+            print("laporancandi hanya dapat diakses oleh akun Bandung Bondowoso.")
     elif prompt == "hancurkancandi":
-            if role == "roro_jonggrang":
-                hancurkancandi()
-            else:
-                print("hancurkancandi hanya dapat diakses oleh akun Roro Jonggrang.")
+        if role == "roro_jonggrang":
+            hancurkancandi()
+        else:
+            print("hancurkancandi hanya dapat diakses oleh akun Roro Jonggrang.")
     elif prompt == "ayamberkokok":
-            if role == "ayamberkokok":
-                ayamberkokok()
-            else:
-                print("ayamberkokok hanya dapat diakses oleh akun Roro Jonggrang.")
+        if role == "ayamberkokok":
+            ayamberkokok()
+        else:
+            print("ayamberkokok hanya dapat diakses oleh akun Roro Jonggrang.")
     elif prompt == "save":
-            save()
+        save()
 
     elif prompt == "help":
-            help()
+        help()
 
     elif prompt == "exit":
-            pass
+        exit()
+        break
     else :
         print("Command yang dimasukkan tidak valid, ketik help untuk list command yang mungkin")
         continue
