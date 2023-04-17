3b7a170a63661ae5244637c7f122f530a5f446bb
Filbert <92862064+Filbert88@users.noreply.github.com>
2023-04-17 21:17:42 +0700

    Update F05_ubahjin.py

--- F05_ubahjin.py
+++ F05_ubahjin.py
@@ -1,4 +1,6 @@
 # (F05) Mendefinisikan Fungsi Ubah Jin 
+from load_data import *
+
 def ubahjin():
     global username_arr, password_arr, role_arr
     unamejin = input("Masukkan username jin : ")
@@ -8,19 +10,21 @@ def ubahjin():
             valid = True
             indeks = i
             break
-    else: valid = False
+    else : 
+        valid = False
     if valid == False :
-        print("Tidak ada jin dengan username tersebut.")
+        print("\nTidak ada jin dengan username tersebut.")
     else :
-        if role_arr[indeks] == "Pengumpul" :
+        if role_arr[indeks] == "jin_pengumpul" :
             ubah = input("Jin ini bertipe \"Pengumpul\". Yakin ingin mengubah ke tipe \"Pembangun\" (Y/N)? ")
             if ubah == "Y" or ubah == "y" :
-                role_arr[indeks] = "Pembangun"
+                role_arr[indeks] = "jin_pembangun"
                 print("\nJin telah berhasil diubah.")
             else : pass
         else :
             ubah = (input("Jin ini bertipe \"Pembangun\". Yakin ingin mengubah ke tipe \"Pengumpul\" (Y/N)? "))
             if ubah == "Y" or ubah =="y" :
-                role_arr[indeks] = "Pengumpul"
+                role_arr[indeks] = "jin_pengumpul"
                 print("\nJin telah berhasil diubah.")
             else : pass
+
