f54b430615bd2741b36b27e12431910ef01528eb
Filbert <92862064+Filbert88@users.noreply.github.com>
2023-04-17 12:46:55 +0700

    Update F07_kumpul.py

--- F07_kumpul.py
+++ F07_kumpul.py
@@ -1,3 +1,17 @@
 # (F07) Mendefinisikan Fungsi Kumpul
+from random import randint
+from util import *
+from load_data import *
+
 def kumpul():
-    pass
+    p = randint(0,5)
+    b = randint(0,5)
+    a = randint(0,5)
+    data_bahan_bangunan[1][2] = int(data_bahan_bangunan[1][2])    
+    data_bahan_bangunan[2][2] = int(data_bahan_bangunan[2][2])
+    data_bahan_bangunan[3][2] = int(data_bahan_bangunan[3][2])
+    data_bahan_bangunan[1][2] += p
+    data_bahan_bangunan[2][2] += b
+    data_bahan_bangunan[3][2] += a
+    print(f"Jin menemukan {p} pasir, {b} batu, dan {a} air.")
+    return data_bahan_bangunan
