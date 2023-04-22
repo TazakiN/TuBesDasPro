98fcb7bd1f5b0fab6a0e47717c94a2cbd9a18cb5
Filbert <92862064+Filbert88@users.noreply.github.com>
2023-04-22 15:46:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from commands import run
 username_arr,password_arr,role_arr,data_candi,data_bahan_bangunan=load()
 # Mendeklarasikan variabel role_arr yang menentukan fungsi yang dapat diakses
 # Melakukan looping untuk terus meminta prompt
-while True:
-    prompt = input(">>> ")
-    run(prompt)
+if username_arr != None and password_arr != None and role_arr != None and data_candi != None and data_bahan_bangunan != None : 
+    while True:
+        prompt = input(">>> ")
+        run(prompt)
