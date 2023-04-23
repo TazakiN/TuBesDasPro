0cf9ec4150c9d48d4ebf47c82549fde1e1392b9c
Filbert <92862064+Filbert88@users.noreply.github.com>
2023-04-23 22:56:05 +0700

    Update F06_bangun.py

--- F06_bangun.py
+++ F06_bangun.py
@@ -3,50 +3,54 @@ from load_data import *
 from random import randint
 
 # (F06) Mendefinisikan Fungsi Bangun.
-def bangun(uname):
-    global data_bahan_bangunan, data_candi
+# (F06) Mendefinisikan Fungsi Bangun
+# Import
+import random
 
-    # Mendapatkan data banyaknya bahan bangunan dari Array of Array of String data_bahan_bangunan.
+# (F06) Mendefinisikan Fungsi Bangun
+def bangun(uname, data_bahan_bangunan, data_candi) -> None:
+    print(data_bahan_bangunan)
+    # Mendatkan data banyaknya bahan bangunan dari Array of Array of String data_bahan_bangunan
     sum_pasir = int(data_bahan_bangunan[1][2])
     sum_batu = int(data_bahan_bangunan[2][2])
     sum_air = int(data_bahan_bangunan[3][2])
 
     # Men-generate angka random untuk menentukan bahan yang dibutuhkan untuk membuat candi.
-    need_bahan = [randint(1,5) for i in range(3)]
-    
-    # Menggagalkan pembangunan candi jika bahan tidak mencukupi.
+    need_bahan = [random.randint(1,5) for i in range(3)]
+    print(need_bahan)
+    # Menggagalkan pembangunan candi jika bahan tidak mencukupi
     if need_bahan[0] > sum_pasir or need_bahan[1] > sum_batu or need_bahan[2] > sum_air:
         print("Bahan Bangunan tidak mencukupi.")
         print("Candi tidak bisa dibangun!")
-        
-    else:   # Saat bahan mencukupi kebutuhan untuk membuat satu candi.
-        # Mengurangi bahan yang dimiliki pada variabel sum sesuai kebutuhan bahan.
+    else:   # Saat bahan mencukupi kebutuhan untuk membuat satu candi
+        # Mengurangi bahan yang dimiliki pada variabel sum sesuai kebutuhan bahan
         sum_pasir -= need_bahan[0]
         sum_batu -= need_bahan[1]
         sum_air -= need_bahan[2]
-
-        # Merubah banyak bahan pada Array of Array of String data_bahan_bangunan.
+        print(need_bahan)
+        # Merubah banyak bahan pada Array of Array of String data_bahan_bangunan
         data_bahan_bangunan[1][2] = str(sum_pasir)
         data_bahan_bangunan[2][2] = str(sum_batu)
         data_bahan_bangunan[3][2] = str(sum_air)
 
-        # Membangun candi pada Array of Array of String data_candi.
+        # Membangun candi pada Array of Array of String data_candi
         for iter in range(101):
             if data_candi[iter] == None:
-                # Melakukan perubahan data pada Array data_candi di index [iter].
                 data_candi[iter] = ["" for i in range(5)]
-                data_candi[iter][0] = str(iter)
+                data_candi[iter][0] = iter
                 data_candi[iter][1] = uname
                 data_candi[iter][2] = str(need_bahan[0])
                 data_candi[iter][3] = str(need_bahan[1])
                 data_candi[iter][4] = str(need_bahan[2])
-
-                # Menampilkan output ke layar.
+                print(data_candi)
                 print("Candi berhasil dibangun.")
                 print("Sisa candi yang perlu dibangun:", 100 - iter)
                 break
             if iter == 100:
-                # Menampilkan output ke layar tanpa merubah data pada array data_candi.
                 print("Candi berhasil dibangun")
                 print("Sisa candi yang perlu dibangun: 0")
+ 
+    return uname, data_bahan_bangunan, data_candi
+
+   
 
