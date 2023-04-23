57723803555626db5c07d6ec21d55071af0413e1
Filbert <92862064+Filbert88@users.noreply.github.com>
2023-04-24 01:40:27 +0700

    Update F04_hapusjin.py

--- F04_hapusjin.py
+++ F04_hapusjin.py
@@ -2,18 +2,24 @@
 from load_data import *
 
 def hapusjin():
-    unamejin = input("Masukkan username: ")
+    unamejin = input("Masukkan username jin : ")
     for i in range(102):
-        if username_arr[i]==unamejin:
-            valid=True
-            index=i
+        if username_arr[i] == unamejin:
+            valid = True
+            index = i
             break
     else : 
         valid = False
     if valid == False :
-        print("Tidak ada jin dengan username tersebut.")
+        print("\nTidak ada jin dengan username tersebut.")
     else :
-        username_arr[index] = None
-        role_arr[index] = None
-        password_arr[index] = None
-        print(f"Jin dengan username {unamejin} telah  dihapus")
+        hapus = input(f"Apakah anda yakin ingin menghapus jin dengan username {unamejin} (Y/N)? ")
+        if hapus == "Y" :
+            username_arr[index] = None
+            role_arr[index] = None
+            password_arr[index] = None
+            print(f"\nJin telah dihapus dari alam gaib.")
+            for i in range (1,101):
+                if data_candi != None and data_candi[i][1] == unamejin:
+                    data_candi[i] = None
+        else : print("\nJin tidak dihapus dari alam gaib.")
