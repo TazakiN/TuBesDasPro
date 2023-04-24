2deb8b0478ff10fb9b65c78b394ea562e55a3069
akmalhfzh <118372685+akmalhfzh@users.noreply.github.com>
2023-04-24 11:36:24 +0700

    Update F11_hancurkancandi.py

--- F11_hancurkancandi.py
+++ F11_hancurkancandi.py
@@ -1,3 +1,13 @@
 # (F11) Mendefinisikan Fungsi Hancurkan Candi
 def hancurkancandi():
-    pass
+    id = int(input("Masukkan ID candi: "))
+
+    if 0 < id < len(data_candi):
+        verif = input(f"Apakah anda yakin ingin menghancurkan candi ID: {id} (Y/N)?")
+        if verif == 'Y' or verif == 'y':
+            del data_candi[id] 
+            print("\nCandi telah berhasil dihancurkan.")
+        else:
+            print("\nCandi batal dihancurkan.")
+    else :
+        print("\nTidak ada candi dengan ID tersebut.")
