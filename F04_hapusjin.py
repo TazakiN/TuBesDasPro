0250c9c7bb8abf7690592ec477a4ffd44a7353f9
Filbert <92862064+Filbert88@users.noreply.github.com>
2023-04-28 01:06:22 +0700

    Update F04_hapusjin.py

--- F04_hapusjin.py
+++ F04_hapusjin.py
@@ -1,25 +1,26 @@
 # (F04) Mendefinisikan Fungsi Hapus Jin
 from load_data import *
 
-def hapusjin():
+def hapusjin(data_candi : list ,username_arr : str) -> None :
     unamejin = input("Masukkan username jin : ")
-    for i in range(102):
+    for i in range(102): # looping untuk mengecek apakah username terdapat di dalam array, jika ada maka valid = True
         if username_arr[i] == unamejin:
             valid = True
-            index = i
+            index = i # index disini nanti akan digunakan ketika ingin menghapus jin karena kita ingin mencari jin yang akan dihapus terletak dimana
             break
     else : 
         valid = False
     if valid == False :
         print("\nTidak ada jin dengan username tersebut.")
-    else :
+    else : # jika valid = True
         hapus = input(f"Apakah anda yakin ingin menghapus jin dengan username {unamejin} (Y/N)? ")
-        if hapus == "Y" :
-            username_arr[index] = None
+        if hapus == "Y" : #  menghapus jin 
+            username_arr[index] = None # maka username, pass,dan role dari jin tersebut akan dihapus dengan diubah menjadi None
             role_arr[index] = None
             password_arr[index] = None
             print(f"\nJin telah dihapus dari alam gaib.")
-            for i in range (1,101):
-                if data_candi[i] != None and data_candi[i][1] == unamejin:
-                    data_candi[i] = None
-        else : print("\nJin tidak dihapus dari alam gaib.")
+            for i in range(1,101):
+                if data_candi[i] != None and data_candi[i][1] == unamejin: # untuk ketika jin tersebut membangun candi dan kita ingin menhapus jin
+                    data_candi[i]=None  # maka data_candi diubah menjadi None
+        else : # jika hapus != Y
+            print("\nJin tidak dihapus dari alam gaib.")
