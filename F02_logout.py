a23ec6c0f6679669e653eeec186e47d9c93f3ee3
Filbert <92862064+Filbert88@users.noreply.github.com>
2023-04-28 01:04:15 +0700

    Update F02_logout.py

--- F02_logout.py
+++ F02_logout.py
@@ -1,8 +1,12 @@
 # (F02) Mendefinisikan Fungsi Save
-def logout(role) :
+
+def logout(role : str) -> str :
+    # jika sudah login artinya role sudah terisi, maka agar bisa logout role akan dikosongin kembali
     if role != "" :
         role = ""
-    else :
+        print("Anda telah keluar dari akun...\nSilakan login kembali jika perlu.")
+    else : # jika role sudah kosong, maka tidak bisa logout lagi sampai role sudah terisi kembali
         print("Logout gagal!\nAnda belum login, silahkan login terlebih dahulu sebelum melakukan logout")
     return role
+
 
