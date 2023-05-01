183fdb733d33c77bc38bd5fc858fe203bdd517dc
Filbert <92862064+Filbert88@users.noreply.github.com>
2023-05-01 16:23:33 +0700

    Update F11_hancurkancandi.py

--- F11_hancurkancandi.py
+++ F11_hancurkancandi.py
@@ -12,9 +12,11 @@ def hancurkancandi(data_candi):
                 print("\nCandi telah berhasil dihancurkan.")
                 for j in range(i+1, 101):  # untuk menggantikan data yang sudah dihilangkan dengan data selanjutnya
                     if data_candi[j] != "%":
-                        data_candi[i] = data_candi[j]
-                        data_candi[j] = "%"
-                        return data_candi
+                        data_candi[j-1], data_candi[j] = data_candi[j],data_candi[j-1]
+                    else :
+                        print(len(data_candi))
+                        data_candi[j], data_candi[j-1] = data_candi[j-1],data_candi[j]
+                        return data_candi 
             else : # jika verif != Y
                 print("\nCandi batal dihancurkan.")
                 return data_candi
