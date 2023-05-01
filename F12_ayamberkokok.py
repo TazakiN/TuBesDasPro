7ce47684751bb424c50b399ecc3fb2587f9ef74e
Filbert <92862064+Filbert88@users.noreply.github.com>
2023-05-02 02:35:03 +0700

    Update F12_ayamberkokok.py

--- F12_ayamberkokok.py
+++ F12_ayamberkokok.py
@@ -5,7 +5,7 @@ import sys
 # F12 - Ayam Berkokok
 # Akses : Roro Jongrang
 
-def ayamberkokok():
+def ayamberkokok(data_candi):
     print("Kukuruyuk.. Kukuruyuk..\n")
     count = 0   # inisialisasi count 
     for i in range(1,101):
@@ -13,7 +13,6 @@ def ayamberkokok():
             count += 1    # menghitung jumlah candi yang sudah dibangun
         else : pass 
     print(f"Jumlah Candi: {count}\n") 
-    tes = 0
     for i in range(1,101) :
         if data_candi[i] != "%" :
             if count == 100 : # jika jumlah candi 100 maka Bandung bondowoso memenangkan permainan
@@ -23,3 +22,4 @@ def ayamberkokok():
                 print("Selamat, Roro Jonggrang memenangkan permainan!\n")
                 print("*Bandung Bondowoso angry noise*\nRoro Jonggrang dikutuk menjadi candi.") # jika jumlah candi < 100 maka roro jonggrang memenangkan permainan
                 sys.exit(1)
+    return data_candi
